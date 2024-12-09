53d956497a93e8f18a60e2c52d6d93c1cdfdf2cf
Vasyl <vasya.chornous@gmail.com>
2024-12-09 21:16:17 +0200

    fix signup issue

--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -1,8 +1,15 @@
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth, firestore } from "../firebase/firebase";
-import { doc, setDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
 import useShowToast from "./useShowToast";
-import useAuthStore from "../store/authSore";
+import useAuthStore from "../store/authStore";
 const useSignUpWithEmailAndPassword = () => {
   const [createUserWithEmailAndPassword, , loading, error] =
     useCreateUserWithEmailAndPassword(auth);
@@ -19,6 +26,17 @@ const useSignUpWithEmailAndPassword = () => {
       showToast("Error", "Please fill all the fields", "error");
       return;
     }
+
+    const usersRef = collection(firestore, "users");
+
+    const q = query(usersRef, where("username", "==", inputs.username));
+    const querySnapshoot = await getDocs(q);
+
+    if (!querySnapshoot.empty) {
+      showToast("Error", "Username already exist", "error");
+      return;
+    }
+
     try {
       const newUser = await createUserWithEmailAndPassword(
         inputs.email,
